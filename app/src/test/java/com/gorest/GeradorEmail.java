8a7a8c33dbba486f3ca450b85372ba60093fff2e
Carolina <chmqatester@gmail.com>
2023-05-31 15:49:39 -0300

    reafatorando codigo

---
+++ app/src/test/java/com/gorest/GeradorEmail.java
@@ -0,0 +1,26 @@
+package com.gorest;
+
+import java.util.Random;
+
+public class GeradorEmail {
+    public static String generateRandomEmail() {
+        String[] domains = {"example.com", "test.com", "dummy.net"}; // Lista de provedores de email fictícios
+        int length = 10; // Comprimento desejado do nome do email
+
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        // Gera uma sequência aleatória de caracteres alfanuméricos para o nome do email
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(36);
+            char c = (char) (index < 26 ? 'a' + index : '0' + index - 26);
+            sb.append(c);
+        }
+
+        // Adiciona um provedor de email aleatório ao nome do email gerado
+        String domain = domains[random.nextInt(domains.length)];
+        sb.append("@").append(domain);
+
+        return sb.toString();
+    }
+}
---
+++ app/src/test/java/com/gorest/dominio/Usuario.java
@@ -0,0 +1,18 @@
+package com.gorest.dominio;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Usuario {
+
+    private String name;
+    private String gender;
+    private String email;
+    private String status;
+}
